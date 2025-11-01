2add98625b64cdd7c61bda453bbea51f6ea782c3
Anastasiia Istomina <anastasiia.istomina@post.runi.ac.il>
2025-11-01 11:36:08 +0200

    Completed assignment

--- Bill3.java
+++ Bill3.java
@@ -8,7 +8,7 @@ public class Bill3 {
 
 		Double pay = (double)bill / 3;
 
-		System.out.println("Dear " + name3 + ", " + name2 + " and " +
-		                   name1 + " pay " + Math.ceil(pay) + " Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + " and: " +
+		                   name1 + " pay " + Math.ceil(pay) + " Shekels each");
 	}
 }						   
